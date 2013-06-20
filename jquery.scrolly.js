c41e999348a077c98ff054a314677c0461d0a20e
ajpinto <aj@isimplifyme.com>
2013-06-20 15:32:51 -0500

    Update jquery.scrolly.js
    Added horizontal parallax suport to allow elements to move left and right as well as up and down allowing for diagonal parallax movement.

--- jquery.scrolly.js
+++ jquery.scrolly.js
@@ -2,6 +2,7 @@
  *  Project: Scrolly : parallax is easy as a matter of fact !
  *  Description: Based on jQuery boilerplate
  *  Author: Victor C. / Octave & Octave web agency
+ *  Forked By: AJ Pinto
  *  Licence: MIT
  */
 (function ( $, window, document, undefined ) {
@@ -15,6 +16,7 @@
     function Plugin( element, options ) {
         this.element = element;
         this.$element = $(this.element);
+    	this.parent = this.$element.parent(this);
 
         this.options = $.extend( {}, defaults, options) ;
         
@@ -26,11 +28,16 @@
 
     Plugin.prototype.init = function () {
         var self = this;
-        this.startPosition = this.$element.position().top;
+        this.vStartPosition = this.$element.position().top;
+        this.hStartPosition = this.$element.position().left;
         this.offsetTop = this.$element.offset().top;
+        this.offsetLeft = this.$element.offset().left;
         this.height = this.$element.outerHeight(true);
-        this.velocity = this.$element.attr('data-velocity');
-        this.bgStart = parseInt(this.$element.attr('data-fit'), 10);
+        this.width = this.$element.outerWidth(true);
+        this.verVel = this.$element.attr('data-vervel');
+        this.horVel = this.$element.attr('data-horvel');
+        this.bgVStart = parseInt(this.$element.attr('data-vfit'), 10);
+        this.bgHStart = parseInt(this.$element.attr('data-hfit'), 10);
 
         $(document).scroll(function(){
             self.didScroll = true;
@@ -45,26 +52,36 @@
     };
 
     Plugin.prototype.scrolly = function() {
-        var dT =  $(window).scrollTop(),
-            wH = $(window).height(),
-            position = this.startPosition;
+        var dT = $(window).scrollTop(),
+            wH = this.parent.height(),
+            wW = this.parent.width(),
+            vPosition = this.vStartPosition;
+            hPosition = this.hStartPosition;
 
-        if(this.offsetTop >= (dT+wH)) {
+        if(this.offsetTop >= (dT+wH) && this.offsetLeft >= (dL+wW)) {
             this.$element.addClass('scrolly-invisible');
         } else {
             if(this.$element.hasClass('scrolly-invisible')){
-                position = this.startPosition + (dT + ( wH - this.offsetTop ) ) * this.velocity;
+                vPosition = this.vStartPosition + (dT + ( wH - this.offsetTop ) ) * this.verVel;
+                hPosition = this.hStartPosition + (dT + ( wW - this.offsetLeft ) ) * this.horVel;
             } else {
-                position = this.startPosition + dT  * this.velocity;
+                vPosition = this.vStartPosition + dT  * this.verVel;
+                hPosition = this.hStartPosition + dT  * this.horVel;
             }
         }
         // Fix background position
-        if(this.bgStart){ position = position + this.bgStart; }
+        if(this.bgVStart){
+			vPosition = vPosition + this.bgVStart;
+		}
+        if(this.bgHStart){
+			hPosition = hPosition + this.bgHStart;
+		}
 
         if(this.options.bgParallax === true) {
-            this.$element.css({backgroundPosition: '50% '+position+'px'});
+            this.$element.css({backgroundPosition: hPosition+'px '+vPosition+'px'});
         } else {
-            this.$element.css({top: position});
+            this.$element.css({top: vPosition});
+            this.$element.css({left: hPosition});
         }
     };
 
